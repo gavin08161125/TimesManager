450d6420b4e676d9afdf4337dd57ea917e825d41
Torssworks <56017301+Torssworks@users.noreply.github.com>
2021-01-09 00:48:25 +0800

    create Tasks files
    Yes

---
+++ app/Task.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property integer $id
+ * @property int $pro_id
+ * @property string $name
+ * @property string $startingtime
+ * @property string $deadline
+ * @property int $totaltime
+ * @property string $picker
+ * @property string $created_at
+ * @property string $updated_at
+ */
+class Task extends Model
+{
+    /**
+     * The "type" of the auto-incrementing ID.
+     * 
+     * @var string
+     */
+    protected $keyType = 'integer';
+
+    /**
+     * @var array
+     */
+    protected $fillable = ['pro_id', 'name', 'startingtime', 'deadline', 'totaltime', 'picker', 'created_at', 'updated_at'];
+
+}
