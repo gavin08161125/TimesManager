7e092a9ef07eaeebaa287ea320e16a1c6598f234
gavin <60091038+gavin08161125@users.noreply.github.com>
2021-01-17 23:00:10 +0800

    2021-1-17
    F

--- resources/views/admin/project/index.blade.php
+++ resources/views/admin/project/index.blade.php
@@ -54,12 +54,12 @@
                     <th>專案開始時間</th>
                     <th>專案截止時間</th>
                     <th>專案總計時間</th>
-                    <th>描述</th>
-                    <th>專案任務總計</th>
-                    <th>已完成任務總計</th>
-                    <th>未完成任務總計</th>
+                    {{-- <th>描述</th> --}}
+                    {{-- <th>專案任務總計</th> --}}
+                    {{-- <th>已完成任務總計</th> --}}
+                    <th>未完成任務</th>
                     <th>擁有者</th>
-                    <th>功能</th>
+                    <th>功能 </th>
                 </tr>
             </thead>
             <tbody>
@@ -68,7 +68,7 @@
 
                 <tr>
                     {{-- 標題 --}}
-                    <td><a href="/admin/project/project_datail/{{$project->id}}">{{$project->title}}</a></td>
+                    <td>{{$project->title}}</td>
                     {{-- 專案開始時間 --}}
                     <td>{{$project->startingtime}}</td>
                     {{-- 專案截止時間 --}}
@@ -81,31 +81,84 @@
                         @elseif(floor((strtotime($project->deadline)-strtotime($project->startingtime))/3600)+1 >0 &&
                         floor((strtotime($project->deadline)-strtotime($project->startingtime))/3600)+1 <= 1 )
                             {{floor((strtotime($project->deadline)-strtotime($project->startingtime))/60 )}} 分鐘 @else
-                            請檢查專案開始時間與截止時間是否正確 @endif </td> <td>{{$project->description}}</td>
+                            請檢查專案開始時間與截止時間是否正確 @endif </td>
+                    {{-- 描述 --}}
+                    {{-- <td>{{$project->description}}</td> --}}
                     {{-- 任務總計 --}}
-                    <td>{{count($project->tasks)}}</td>
+                    {{-- <td>{{count($project->tasks)}}</td> --}}
                     {{-- 已完成任務總計 --}}
-                    <td>{{count($project->tasks->where('status','2'))}}</td>
+                    {{-- <td>{{count($project->tasks->where('status','2'))}}</td> --}}
                     {{-- 未完成任務總計 --}}
                     <td>{{count($project->tasks->where('status','1'))}}</td>
                     {{-- 擁有者 --}}
                     <td>{{$project->owner}}</td>
                     {{-- 功能 --}}
                     <td>
-                        <a class='btn btn-success btn-sm  @if($project->status == 2) disabled @endif ' href="/admin/project/edit/{{$project->id}}" >編輯
+                        <a class='btn btn-success btn-sm  @if($project->status == 2) disabled @endif '
+                            href="/admin/project/edit/{{$project->id}}">編輯
                         </a>
                         <a class='btn btn-danger btn-sm' href="/admin/project/destroy/{{$project->id}}"
                             onclick="javascript:return del();">刪除
                         </a>
-                        <a class='btn btn-success btn-sm @if($project->status == 2) disabled @endif ' href="/admin/project/add_member/edit/{{$project->id}}"  >新增成員
-                        </a>
-                        <a class='btn btn-danger btn-sm @if($project->status == 2) disabled @endif' href="/admin/project/delete_select/{{$project->id}}"  >刪除成員
-                        </a>
-                        <form action="/admin/project/task/{{$project->id}}">
-                            <button class='btn btn-success btn-sm' name="id" value="{{$project->id}}" >工作管理</button>
-                        </form>
-                        <a  class='btn btn-danger btn-sm @if($project->status == 2) disabled @endif' href="/admin/project/endProject/{{$project->id}}"   onclick="javascript:return endProject();">封存專案</a>
+                        {{-- <a class='btn btn-success btn-sm @if($project->status == 2) disabled @endif '
+                            href="/admin/project/add_member/edit/{{$project->id}}">新增成員
+                        </a> --}}
+                        {{-- <a class='btn btn-danger btn-sm @if($project->status == 2) disabled @endif'
+                            href="/admin/project/delete_select/{{$project->id}}">刪除成員
+                        </a> --}}
+                        {{-- <form action="/admin/project/task/{{$project->id}}">
+                            <button class='btn btn-success btn-sm' name="id" value="{{$project->id}}">工作管理</button>
+                        </form> --}}
+                        <a  href="/admin/project/task/{{$project->id}}" class='btn btn-success btn-sm' name="project_id" data-project_id="{{$project->id}}" >工作管理</a>
+
+                        <button type="button" class="btn btn-success btn-sm" data-toggle="modal"
+                            data-target="#exampleModalLong{{$project->id}}">
+                            專案詳細
+                        </button>
                     </td>
+
+                    <div class="modal fade" id="exampleModalLong{{$project->id}}" tabindex="-1" role="dialog"
+                        aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+                        <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="exampleModalLongTitle">專案詳細</h5>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <div class="modal-body">
+                                    <h5>專案名稱：{{$project->title}}</h5>
+                                    <hr>
+                                    <h5>任務總計：{{count($project->tasks)}}</h5>
+                                    <hr>
+                                    <h5>已完成任務總計：{{count($project->tasks->where('status','2'))}}</h5>
+                                    <hr>
+                                    <h5>未完成任務總計：{{count($project->tasks->where('status','1'))}}</h5>
+                                    <hr>
+                                    <h5>描述</h5>
+                                    <textarea cols="55" rows="10" readonly>{{$project->description}}</textarea>
+                                </div>
+                                <div class="modal-footer">
+
+                                    <a class='btn btn-success  @if($project->status == 2) disabled @endif '
+                                        href="/admin/project/add_member/edit/{{$project->id}}">新增成員
+                                    </a>
+
+                                    <a class='btn btn-danger  @if($project->status == 2) disabled @endif'
+                                        href="/admin/project/delete_select/{{$project->id}}">刪除成員
+                                    </a>
+
+                                    <a class='btn btn-danger  @if($project->status == 2) disabled @endif'
+                                        href="/admin/project/endProject/{{$project->id}}"
+                                        onclick="javascript:return endProject();">封存專案</a>
+
+
+                                    <button type="button" class="btn btn-secondary " data-dismiss="modal">關閉</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
 
                 </tr>
                 @endforeach
@@ -124,8 +177,13 @@
     @section('js')
 
     <!-- #region datatables files -->
-
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"
+        integrity="sha512-bLT0Qm9VnAYZDflyKcBaQ2gg0hSYNQrJ8RilYldYQ1FxQYoCLtUjuuRuZo+fjqhx/qtq/1itJ0C2ejDxltZVFg=="
+        crossorigin="anonymous"></script>
     <script src="https://cdn.datatables.net/1.10.23/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.6.0/umd/popper.min.js"
+        integrity="sha512-BmM0/BQlqh02wuK5Gz9yrbe7VyIVwOzD1o40yi1IsTjriX/NGF37NyXHfmFzIlMmoSIBXgqDiG1VNU6kB5dBbA=="
+        crossorigin="anonymous"></script>
 
     <script>
         $.noConflict();
