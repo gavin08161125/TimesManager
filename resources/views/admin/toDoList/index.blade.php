088b4204ab1f00d60e7b009a9f05034e50e14d7f
Erwin532 <76420928+Erwin532@users.noreply.github.com>
2021-01-20 22:20:09 +0800

    Frontend renew
    1.按鈕更新
    2.工作日誌更改為並排(繼續尋找漂亮的框中)

--- resources/views/admin/toDoList/index.blade.php
+++ resources/views/admin/toDoList/index.blade.php
@@ -23,77 +23,86 @@
         {{ session('alert') }}
     </div>
     @endif
-    <h2>新增事項</h2>
-    <hr>
-    <form action="/admin/to_do_list/create/{{auth()->user()->id}}" class="hidden_text" method="post">
-        @csrf
-        <div class="form-group">
-            <label for="title">標題</label>
-            <input type="text" class="form-control" id="title" name="title" required>
+
+    <div class="row">
+        <div class="col-6">
+            <h2>新增事項</h2>
+            <hr>
+            <form action="/admin/to_do_list/create/{{auth()->user()->id}}" class="hidden_text" method="post">
+                @csrf
+                <div class="form-group">
+                    <label for="title">標題</label>
+                    <input type="text" class="form-control" id="title" name="title" required>
+                </div>
+
+                <div class="form-group">
+                    <label for="content">內文</label>
+                    <textarea class="form-control" id="content" rows="3" name="content" required></textarea>
+                </div>
+
+                <button class="btn btn-primary">新增</button>
+
+            </form>
+
         </div>
+        <div class="col-6">
+            @foreach ($myLists as $myList)
+            <div class="list-group">
+                <button type="button" class="btn btn-success btn-sm" data-toggle="modal"
+                    data-target="#exampleModalLong{{$myList->id}}">
+                    事項詳細
+                </button>
+                <a href="#" data-toggle="modal" class="list-group-item list-group-item-action flex-column align-items-start ">
+                    <div class="d-flex w-100 justify-content-between">
+                        <h5 class="mb-1">{{$myList->title}}</h5>
+                        <small>{{$myList->created_at}}</small>
 
-        <div class="form-group">
-            <label for="content">內文</label>
-            <textarea class="form-control" id="content" rows="3" name="content" required></textarea>
-        </div>
-
-        <button class="btn btn-primary">新增</button>
-
-    </form>
-
-    @foreach ($myLists as $myList)
-    <div class="list-group">
-        <button type="button" class="btn btn-success btn-sm" data-toggle="modal"
-            data-target="#exampleModalLong{{$myList->id}}">
-            事項詳細
-        </button>
-        <a href="#" data-toggle="modal" class="list-group-item list-group-item-action flex-column align-items-start ">
-            <div class="d-flex w-100 justify-content-between">
-                <h5 class="mb-1">{{$myList->title}}</h5>
-                <small>{{$myList->created_at}}</small>
-
-            </div>
-            <p class="mb-1">{{$myList->content}}</small>
-
-
-        </a>
-
-        <div class="modal fade" id="exampleModalLong{{$myList->id}}" tabindex="-1" role="dialog"
-            aria-labelledby="exampleModalLongTitle" aria-hidden="true">
-            <div class="modal-dialog" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLongTitle">編輯事項</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
                     </div>
-                    <form action="/admin/to_do_list/update/{{$myList->id}}">
-                        <div class="modal-body">
-                            <h5>創建日期：{{$myList->created_at}}</h5>
-                            <hr>
-                            <h5>標題:</h5><input type="text" class="form-control" id="title" name="title"
-                                value="{{$myList->title}}" required>
-                            <hr>
+                    <p class="mb-1">{{$myList->content}}</small>
 
-                            <h5>描述:</h5>
-                            <textarea class="form-control" id="content" rows="3" name="content"
-                                required>{{$myList->content}}</textarea>
+
+                </a>
+
+                <div class="modal fade" id="exampleModalLong{{$myList->id}}" tabindex="-1" role="dialog"
+                    aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLongTitle">編輯事項</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <form action="/admin/to_do_list/update/{{$myList->id}}">
+                                <div class="modal-body">
+                                    <h5>創建日期：{{$myList->created_at}}</h5>
+                                    <hr>
+                                    <h5>標題:</h5><input type="text" class="form-control" id="title" name="title"
+                                        value="{{$myList->title}}" required>
+                                    <hr>
+
+                                    <h5>描述:</h5>
+                                    <textarea class="form-control" id="content" rows="3" name="content"
+                                        required>{{$myList->content}}</textarea>
+                                </div>
+                                <div class="modal-footer">
+                                    <button class='btn btn-success'>儲存</button>
+                                    <a class='btn btn-danger ' href="/admin/to_do_list/destroy/{{$myList->id}}">刪除</a>
+                                    <button type="button" class="btn btn-secondary " data-dismiss="modal">關閉</button>
+                                </div>
+                            </form>
+                            </button>
                         </div>
-                        <div class="modal-footer">
-                            <button class='btn btn-success'>儲存</button>
-                            <a class='btn btn-danger ' href="/admin/to_do_list/destroy/{{$myList->id}}">刪除</a>
-                            <button type="button" class="btn btn-secondary " data-dismiss="modal">關閉</button>
-                        </div>
-                    </form>
-                    </button>
+                    </div>
                 </div>
             </div>
+            @endforeach
+            <button class='btn btn-success add_project'>新增</button>
+            <button class="btn btn-success remove_add">取消新增</button>
         </div>
     </div>
-    @endforeach
-    <button class='btn btn-success add_project'>新增</button>
-    <button class="btn btn-success remove_add">取消新增</button>
+
+
 </div>
 
 
